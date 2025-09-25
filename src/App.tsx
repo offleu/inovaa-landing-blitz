9456702dcc3fec930659174a25890c821898f495
Leonardo R. Lima <140715306+offleu@users.noreply.github.com>
2025-09-24 21:08:24 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import FormularioContato from "./pages/FormularioContato";
-import Blog from "./pages/Blog";
+import Blog from "./pages/blog";
 import ArtigoEcommerce from "./pages/ArtigoEcommerce";
 import NotFound from "./pages/NotFound";
 
